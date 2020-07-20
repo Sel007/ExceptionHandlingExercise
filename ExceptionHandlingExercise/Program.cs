f7b0230a0991e1f290185ecb51d11ad06377c81d
Sel007 <selina.08225.1@gmail.com>
2020-07-19 22:00:13 -0400

    Added CW(ex.Message)

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -36,26 +36,26 @@ namespace ExceptionHandlingExercise
                 //DONE - Now create a try catch
                 try
                 {
+                //DONE
+                // Inside your try block
+                // set your string variable to each array element in your char[] to .ToString()
+                // Now, using int.Parse, "parse your string variable" and store in an "int variable"
+                // Then add each int to your list
                     str = character.ToString();
                     int num = int.Parse(str);
                     numbers.Add(num);
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Unable to Parse '{character}'");
-                }
-
-                //DONE
-                // Inside your try block
-                // set your string variable to each array element in your char[] to .ToString()
-                // Now, using int.Parse, "parse your string variable" and store in an "int variable"
-                // Then add each int to your list
+                //DONE               
                 // catch your Exception:
                 // in the scope of your catch you can use the following, 
-                //DONE               
                 //Console.WriteLine($"Unable to Parse '{character}'"); 
-                //character will be the:
-                //name of each item in your collection
+                //character will be the name of each item in your collection
+                    Console.WriteLine($"Unable to Parse '{character}'");
+                    Console.WriteLine(ex.Message);
+                }
+
             }
 
             foreach (var num in numbers)
